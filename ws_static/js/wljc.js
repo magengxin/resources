f2dd81ad63a88c99bc8ac2113c5c5ae433bb0ed6
MaGengxin <1161807772@qq.com>
2019-08-24 17:28:13 +0800

    '修改背景样式'

--- ws_static/js/wljc.js
+++ ws_static/js/wljc.js
@@ -1,9 +1,9 @@
 window.onload = function () {
   var num = 1;
-  var flag = false;
 
   $('.item-handle').click(function () {
-    if (flag) {
+    var index = $(this).parent().index();
+    if (index < num) {
       if ($(this).siblings("ul").hasClass('on')) {
         $(this).siblings("ul").slideUp(500).removeClass("on");
       } else {
@@ -14,34 +14,38 @@ window.onload = function () {
 
       }
 
-      $(this).parent('.ul-box').siblings().removeClass("active");
+      // $(this).parent('.ul-box').siblings().removeClass("active");
       // 添加 active
-      $(this).parent('.ul-box').addClass("active");
+      // $(this).parent('.ul-box').addClass("active");
     }
   });
   $('.paging-arrow-down').click(function () {
-    var dataBox = $('.wljc-list').children('.ul-box');
-    var len = dataBox.length;
+    var ulBox = $('.wljc-list').children('.ul-box');
+    var len = ulBox.length;
 
     num++;
-    // 禁用按钮
-    if (num == len - 1) {
-      dataBox.siblings().removeClass('active').eq(num - 1).addClass('active')
+    console.log("len", len);
 
-      $(this).removeClass('active').addClass('disable');
-      return false
-    } else if (num >= len) {
-      return false
-    }
+
+    // TODO: num 8 按钮没有置灰
+    // TODO: 已经选择的可以完成手风琴效果
+
+    $(this).siblings().removeClass('disable').addClass('active');
+
     console.log("num", num);
 
-    // 其他移除 active ，当前添加 active
-    dataBox.siblings().removeClass('active').eq(num - 1).addClass('active')
+    // 其他移除ul，当前添加 active
+    // ulBox.siblings().removeClass('active').eq(num - 1).addClass('active')
+    ulBox.eq(num - 1).addClass('active')
 
     // 其他ul隐藏，当前显示ul
-    // dataBox.siblings().children("ul").slideDown(500).eq(num - 1).children("ul").slideUp(500);
-    dataBox.siblings().children("ul").removeClass('on').end().eq(num - 1).children("ul").addClass('on');
+    // ulBox.siblings().children("ul").slideDown(500).eq(num - 1).children("ul").slideUp(500);
+    ulBox.siblings().children("ul").slideUp(500).removeClass('on').end().eq(num - 1).children("ul").slideDown(500).addClass('on');
 
-
+    // 禁用按钮
+    if (num >= len) {
+      $(this).removeClass('active').addClass('disable');
+      return false
+    }
   })
 }
